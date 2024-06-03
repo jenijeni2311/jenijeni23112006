39b705f52fe45f8ecd569feb74d59dd1bbdf1158
jenijeni2311 <jeniffermarques05@gmail.com>
2024-06-03 20:09:03 -0300

    adicionando arquivo js e css

---
+++ sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+    createCanvas(400, 400);
+    background("lightgrey")
+  
+    textFont("verdana");
+    textSize(30)
+    textAlign(CENTER,CENTER);
+  
+  }
+  
+  function draw(){
+    text("boa tarde,professoraa",200,200)
